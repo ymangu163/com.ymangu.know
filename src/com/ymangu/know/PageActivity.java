379c4e9485fe1fafdfb280fc5ee7958542d6b53f
ymangu163 <ymangu@163.com>
2014-12-17 22:08:15 +0800

    实现了 SlidingMenu ,基于 ActionBarSherLock 解决了 v7包与ActionBarSherLock冲突问题

--- src/com/ymangu/know/PageActivity.java
+++ src/com/ymangu/know/PageActivity.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.ymangu.know.adapter.ViewPagerAdapter;
@@ -22,6 +23,7 @@ public class PageActivity extends Activity {
 	@ViewInject(R.id.pager)
 	private 	android.support.v4.view.ViewPager viewPager;
 	private LocalActivityManager lam;
+	private SlidingMenu sm;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +36,17 @@ public class PageActivity extends Activity {
 		 lam.dispatchCreate(savedInstanceState);  //这个方法必须被调用不能会崩掉
 		initActivity();
 		
+		//实例化一个slidingmenu
+		sm = new SlidingMenu(this);
+		sm.setMode(SlidingMenu.LEFT);   //slidingmenu出现的屏幕左右方向
+		sm.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);  //设置滑出slidingmenu的地方
+		sm.setShadowDrawable(R.drawable.shadow);//  设置主页面和slidingmenu页面之间的阴影图片
+		sm.setShadowWidthRes(R.dimen.shadow_width);//设置阴影的宽度
+		sm.setBehindOffsetRes(R.dimen.slidingmenu_offset);//设置主页面在slidingmenu上显示的宽度
+//		sm.setBehindWidth(400);//直接设置slidingmenu的宽度
+		sm.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);//将当前的slidingmenu作为activity的一部分显示
 		
+		sm.setMenu(R.layout.navigation_layout);//设置slidingmenu显示的layout
 		
 	}
 	//将滑动的Activity转成view
